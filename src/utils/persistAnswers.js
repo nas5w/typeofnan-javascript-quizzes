1183527232b2dbd011feee01a1bb2149533fa2a1
David Christoffersen <d@vidc.dk>
2019-10-17 12:04:56 -0400

    Yes

--- src/utils/persistAnswers.js
+++ src/utils/persistAnswers.js
@@ -2,28 +2,28 @@ export const persistAnswer = (
     title,
     selectedAnswer,
     correctAnswer
-) => {
+  ) => {
     typeof window !== 'undefined' &&
-        localStorage.setItem(
-            title,
-            JSON.stringify({ selectedAnswer, correctAnswer })
-        );
-};
-
-export const getPersistedAnswer = title => {
+      localStorage.setItem(
+        title,
+        JSON.stringify({ selectedAnswer, correctAnswer })
+      );
+  };
+  
+  export const getPersistedAnswer = title => {
     const item =
-        typeof window !== 'undefined' &&
-        localStorage.getItem(title);
+      typeof window !== 'undefined' &&
+      localStorage.getItem(title);
     if (!item) {
-        return { selectedAnswer: null, correctAnswer: null };
+      return { selectedAnswer: null, correctAnswer: null };
     }
     return JSON.parse(item);
-};
-
-export const clearPersistedAnswer = title => {
+  };
+  
+  export const clearPersistedAnswer = title => {
     typeof window !== 'undefined' &&
-        localStorage.removeItem(title);
-};
+      localStorage.removeItem(title);
+  };
 
 export const clearAllPersistedAnswer => {
     typeof window !== 'undefined' &&
